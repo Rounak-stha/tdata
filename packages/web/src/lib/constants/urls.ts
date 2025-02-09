a8ae32c46c6026479e9182ab0d21869e04b82286
Rounak Shrestha <66557682+Rounak-stha@users.noreply.github.com>
2025-02-09 10:59:27 +0545

    Redirect to Active Org if Exists

--- packages/web/src/lib/constants/urls.ts
+++ packages/web/src/lib/constants/urls.ts
@@ -1,12 +1,13 @@
 export const PathPrefix = {
-	api: '/api',
-	auth: '/auth'
-} as const
+  api: "/api",
+  auth: "/auth",
+} as const;
 
 export const Paths = {
-	root: '/',
-	signin: `${PathPrefix.auth}/signin`,
-	onboarding: '/onboarding',
-	error: '/error',
-	task: (org: string, taskNumber: string) => `/${org}/${taskNumber}`
-} as const
+  root: "/",
+  signin: `${PathPrefix.auth}/signin`,
+  onboarding: "/onboarding",
+  error: "/error",
+  org: (org: string) => `/${org}`,
+  task: (org: string, taskNumber: string) => `/${org}/${taskNumber}`,
+} as const;
