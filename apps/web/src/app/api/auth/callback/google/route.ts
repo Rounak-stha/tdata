cfdc07af9cc06048e1db9a5c7fdbca6821f0092e
Rounak Shrestha <66557682+Rounak-stha@users.noreply.github.com>
2025-03-08 23:09:28 +0545

    Remove LOg

--- apps/web/src/app/api/auth/callback/google/route.ts
+++ apps/web/src/app/api/auth/callback/google/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { createSupabaseClient } from "@lib/supabase/server/client";
 
 export async function GET(request: Request) {
-s  const { searchParams, origin } = new URL(request.url);
+  const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
