450a3cedcba8c9aefe4f732ef908f7e3de969c0c
Rounak Shrestha <66557682+Rounak-stha@users.noreply.github.com>
2025-06-22 19:05:05 +0545

    Fix Google SignIn
    Update Redirect URL in both Google and Supabase Console

--- apps/web/src/app/api/auth/callback/google/route.ts
+++ apps/web/src/app/api/auth/callback/google/route.ts
@@ -1,31 +1,20 @@
 import { NextResponse } from "next/server";
 // The client you created from the Server-Side Auth instructions
 import { createSupabaseClient } from "@lib/supabase/server/client";
+import { SITE_URL } from "@/lib/constants";
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
+  const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
-  // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get("next") ?? "/";
 
   if (code) {
     const supabase = await createSupabaseClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
-    console.log(error);
     if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`);
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
-      } else {
-        return NextResponse.redirect(`${origin}${next}`);
-      }
+      return NextResponse.redirect(SITE_URL);
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  return NextResponse.redirect(`${SITE_URL}/auth/auth-code-error`);
 }
